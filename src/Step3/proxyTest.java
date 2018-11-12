a2a82a526738e654c899f69cd11c2c19d18cb0d7
bit4 <bit4woo@163.com>
2018-11-12 15:58:01 +0800

    update Step3

--- src/Step3/proxyTest.java
+++ src/Step3/proxyTest.java
@@ -2,35 +2,43 @@ package Step3;
 
 /*
  * 代理模式的简单demo，静态代理
+ * 
+ * 代理的使用场景：某程序员入职公司接手了一个项目，他读源码发现某些地方可以增强（比如在某些函数执行前应该打印日志）。
+ * 如果他直接在原始代码的基础上直接修改容易出错，他的做法是：自己实现一个类，和原始类实现相同的接口（或者继承相同的类），
+ * 通过在方法中引用老程序的方法来实现自己的方法，从而实现增强方法的目的。
  */
 
 public class proxyTest{
 	public static void main(String[] args) {
-		Subject sub=new ProxySubject();
+		//Subject sub = new RealSubject();//场景中得旧代码，老程序员写的。
+		Subject sub = new ProxySubject();//新入职的程序员，自己实现了ProxySubject类，然后改成了这句。来增强老程序的代码。
 		sub.request();
 	}
 }
 
-abstract class Subject
+abstract class Subject//也可以是接口interface
 {//抽象角色：通过接口或抽象类声明真实角色实现的业务方法。
 	//类比网络代理，比如http代理，都支持http协议
     abstract void request();
 }
 
+//老程序员写的代码，实现了需要的主要功能。
 class RealSubject extends Subject
 {//真实角色：实现抽象角色，定义真实角色所要实现的业务逻辑，供代理角色调用。
 	//类比真实的http请求
-       public RealSubject()
+       public RealSubject()//默认构造方法
        {
        }
-      
+       
+       @Override
        public void request()
        {
               System.out.println("From real subject.");
        }
 }
 
-class ProxySubject extends Subject//关键是类的继承
+//新入职程序员实现的类，目的是增强老程序员的代码。
+class ProxySubject extends Subject//关键是类的继承。
 {//代理角色：实现抽象角色，是真实角色的代理，通过真实角色的业务逻辑方法来实现抽象方法，并可以附加自己的操作。
 	//类比通过代理发出http请求，这个代理当然可以对http请求做出任何想要的修改。
     private RealSubject realSubject; //以真实角色作为代理角色的属性
@@ -38,27 +46,28 @@ class ProxySubject extends Subject//关键是类的继承
        public ProxySubject()
        {
        }
- 
-       public void request() //该方法封装了真实对象的request方法
+       
+       @Override
+       public void request() //该方法封装了真实对象的request方法，老程序员的方法。
        {//所谓的“控制”就体现在这里
         preRequest(); 
-              if( realSubject == null )
+        if( realSubject == null )
         {
-                     realSubject = new RealSubject();
-              }
+            realSubject = new RealSubject();
+        }
         realSubject.request(); //此处执行真实对象的request方法
         postRequest();
        }
  
     private void preRequest()
     {
-        //something you want to do before requesting
-    	System.out.println("Do something before requesting");
+        //在请求前做某些处理，比如打印日志，修改请求包等等
+    	System.out.println("Do something before requesting: print log,change request");
     }
  
     private void postRequest()
     {
-        //something you want to do after requesting
-    	System.out.println("Do something after requesting");
+        //在请求后做某些处理，打印日志
+    	System.out.println("Do something after requesting: print log");
     }
 }
--- src/Step3/proxyTest2.java
+++ src/Step3/proxyTest2.java
@@ -5,22 +5,28 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 /*
- * 代理模式的简单demo，动态代理，动态代理利用了反射机制
+ * 动态代理的简单demo，动态代理利用了反射机制
  * 每一个动态代理类都会有一个与之关联的invocation handler。真正的调用是在invocation handler的invoke()方法里完成的。
- * 感谢蝶离飞、廖新喜2为师傅的指导
+ * 感谢蝶离飞、廖新喜2位师傅的指导
  */
 
 public class proxyTest2{
 	public static void main(String[] args) {
 		DynamicSubject sub=new RealDynamicSubject();//之前这里sub的类型是RealDynamicSubject，不对；但是为什么呢？
 		Handler handler = new Handler(sub);
+		
+		//newProxyInstance(ClassLoader loader,Class<?>[] interfaces,InvocationHandler h)
+		//CLassLoader loader:指定动态代理类的类加载器,即生成完成后的代理对象的类加载器
+		//Class<?> interfaces:指定动态代理类需要实现的所有接口，需要被增强的接口列表（数据）
+		//InvocationHandler h: 指定与动态代理类关联的 InvocationHandler对象，具体的增强逻辑
+		
 		DynamicSubject sub2 = (DynamicSubject)Proxy.newProxyInstance(DynamicSubject.class.getClassLoader(), new Class[]{DynamicSubject.class}, handler); 
-		//CLassLoader loader:指定动态代理类的类加载器
-		//Class<?> interfaces:指定动态代理类需要实现的所有接口
-		//InvocationHandler h: 指定与动态代理类关联的 InvocationHandler对象
+
 		DynamicSubject sub3 = (DynamicSubject)Proxy.newProxyInstance(DynamicSubject.class.getClassLoader(), sub.getClass().getInterfaces(), handler);
 		
 		DynamicSubject sub4 = (DynamicSubject)Proxy.newProxyInstance(DynamicSubject.class.getClassLoader(), RealDynamicSubject.class.getInterfaces(), handler);
+		
+		//从上面的调用方法可知，可以对不同的对象使用相同的模式实现来实现其代理，这就是相对静态代理的优势。
 		
 		System.out.println("sub.getClass() = "+sub.getClass());
 		System.out.println("DynamicSubject.class = " +DynamicSubject.class);
@@ -56,22 +62,22 @@ class RealDynamicSubject implements DynamicSubject
  * 处理器
  */
 class Handler implements InvocationHandler{
-	private Object obj; //被代理的对象，不管对象是什么类型；之前声明成RealDynamicSubject，不应该这么做
+	private Object obj; //被代理的对象（也就是老程序员实现的对象），不管对象是什么类型；之前声明成RealDynamicSubject，不应该这么做
     /**
      * 所有的流程控制都在invoke方法中
      * proxy：代理类
-     * method：正在调用的方法
-     * args：方法的参数
+     * method：正在调用的方法，反射机制调用函数所必须！
+     * args：被调用方法的参数列表，反射机制调用函数所必须！
      */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {//接口必须实现的方法，也是逻辑核心
-    	System.out.println("Do something before requesting");
-    	Object xxx = method.invoke(this.obj, args);
-        System.out.println("Do something after requesting");
+    	System.out.println("Do something before requesting: print log");
+    	Object xxx = method.invoke(this.obj, args);//通过反射机制调用老程序员的对象代码。
+        System.out.println("Do something after requesting: print log");
         return xxx;
     }
     public Handler(Object obj) {
-    	//构造函数，把真实角色的实例传递进来,这个代理handler的目的就是处理它
+    	//构造函数，把真实角色的实例传递进来,这个代理handler的目的就是增强它，或者说需要调用它来实现主要的功能。
         this.obj = obj;
     }
 }
